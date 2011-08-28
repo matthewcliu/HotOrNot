19403d67d5930f8dc211e8156c978249fcc267d5
Matthew Liu <matthewliu@gmail.com>
2011-08-27 23:20:18 -0700

    Initial commit hotornot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hotornot::Application.config.secret_token = '5d491c7d5c2e20cadad7f61768c96df6ac80fd0973192b3717f00d1c6d3b9f56f4a4b05cdc852738f8647521812e06b01d9252ed2aaa2ecd920c430f4a754b4a'
